0464eb633419e98f81a23857210e83d156763593
arthurExaud <88190786+arthurExaud@users.noreply.github.com>
2021-11-10 13:12:55 +0000

    Update ships.js

--- js/ships.js
+++ js/ships.js
@@ -92,6 +92,8 @@ class SpawnBotShips extends Ship {
                 this._botPool.push(spawn[0]);
                 this._botShipPool.push(spawn[1]);
             }
+        } else {
+            throw new Error("Model not loaded");
         }
     }
 
